d1796e9eb3f2adbc0859ee3af76cc3e92f642c48
nauman1004 <143635892+nauman1004@users.noreply.github.com>
2026-04-26 16:49:57 +0530

    Removed .env from tracking and added env config

--- config/db.js
+++ config/db.js
@@ -1,17 +1,18 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "mac123@**",
-    database: "school_db",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
     if (err) {
-        console.error("DB Connection Failed:", err);
+        console.error("Database connection failed:", err);
     } else {
-        console.log("MySQL Connected...");
+        console.log("Connected to MySQL");
     }
 });
 
